958fb22d344019c7906db347abf349ecadd2e71c
MengXianBin <mengxianbindev@163.com>
2020-02-28 12:25:00 +0800

    ugly

---
+++ idea/2020/0228/2020_0228_001.rb
@@ -0,0 +1,60 @@
+use_bpm 100
+
+live_loop :ticks do
+  play_pattern_timed [0], 4
+end
+
+live_loop :beats, sync: :ticks do
+  sample_rate = (sample_duration :loop_amen) / 4.0
+  sample :loop_amen, rate: sample_rate
+  sleep 4
+end
+
+define :play_ch do |ch|
+  play_pattern_timed ch, 0.0625, attack: 0.2, sustain: 2, release: 2
+  sleep 3.75
+end
+
+live_loop :chords, sync: :ticks do
+  use_synth :piano
+  with_fx :reverb do
+    play_ch chord(:D3, :m7)
+    play_ch chord(:G2, "7", invert: 2)
+    play_ch chord(:C3, :M7)
+    play_ch chord(:C3, "6")
+  end
+end
+
+define :play_bass do |root|
+  play_pattern [root]; sleep 0.5
+  play_pattern [root]; sleep 1.5
+end
+
+live_loop :bass, sync: :ticks do
+  use_synth :fm
+  play_bass :F2
+  play_bass :G2
+  play_bass :C3
+  play_bass :A2
+end
+
+live_loop :medoly, sync: :ticks do
+  play_pattern_timed [0], 1
+  play_pattern_timed [(ring :E, :A).tick(:medoly)], 0.5
+  play_pattern_timed [:G], 0.5
+  play_pattern_timed [:G], 0.5
+  play_pattern_timed [:E], 0.5
+  play_pattern_timed [:E], 1
+end
+
+define :play_choir do |root|
+  play_pattern_timed [root], 2.5
+  play_pattern_timed [root - 2], 0.5
+  play_pattern_timed [root], 0.5
+  play_pattern_timed [root - 2], 4.5
+end
+
+live_loop :choir, sync: :ticks do
+  play_choir :E5
+  play_choir :D5
+end
