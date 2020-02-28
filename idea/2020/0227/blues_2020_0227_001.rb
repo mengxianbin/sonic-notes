789c227feca08ac79b3997f70be3e8986fe64c2c
MengXianBin <mengxianbindev@163.com>
2020-02-28 16:18:39 +0800

    comments for method references

--- idea/2020/0227/blues_2020_0227_001.rb
+++ idea/2020/0227/blues_2020_0227_001.rb
@@ -5,7 +5,9 @@ template = [:C, :C, :E, :E, :G, :G, :A, :A, :Bb, :Bb, :A, :A, :G, :G, :E, :E]
 roots = [:C, :C, :F, :C, :G, :F, :C, :C]
 
 live_loop :blues do
+  # play_phrase: reference from /util/play
   play_blues = ->() { roots.each { |root| play_phrase root, template, rhythm } }
   play_piano &play_blues
+  # play_guitar: reference from /instrument/guitar/acoustic_guitar
   play_guitar &play_blues
 end
--- util/play/play_phrase.rb
+++ util/play/play_phrase.rb
@@ -1,6 +1,7 @@
 define :play_phrase do |root, template, rhythm|
   offset = root - note(template[0])
   with_fx :reverb do
+    # check_note: reference from /util/note
     play_pattern_timed template.map { |n| check_note(n + offset) }, rhythm
   end
 end
